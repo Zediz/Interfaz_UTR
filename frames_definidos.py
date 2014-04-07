7ef0ef90dff104fefcfec7a000173e26d2dad1bd
Zediz <hiram.shock@gmail.com>
2014-04-07 09:04:12 -0500

    Hasta alarmas digitales
    Ya con instrucciones

--- frames_definidos.py
+++ frames_definidos.py
@@ -4,6 +4,7 @@
 from Tkinter import *
 import cambio_alarmas
 import alarmas_digitales
+import instrucciones
 
 v0 = Tk()
 v0.config(bg = "white")
@@ -11,6 +12,7 @@ v0.title('Alarmas del Transformador')
 v0.geometry('700x500+290+150')
 olas = alarmas_digitales.alarmas_digitales(v0)
 change = cambio_alarmas.cambio_alarmas(v0)
+instrucc = instrucciones.instrucciones(v0)
 
 
 
@@ -32,11 +34,10 @@ pines =Frame(height = 260, width= 297, bg="black")
 pines.place(x = 402, y = 172)
 #***************************************************************
 
-# FRAME DE Instrucciones *********************
-instrucciones = Frame(height=66, width = 698, bg="black")
-instrucciones.place(x=1,y= 433)
 
+instrucc.mainloop()
 change.mainloop()
 olas.mainloop()
 
-mainloop()
+
+v0.mainloop()
