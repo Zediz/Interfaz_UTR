45996efbf390d0091469d01b95beb07d6661c760
Zediz <hiram.shock@gmail.com>
2014-04-23 10:57:15 -0500

    IGHMUTRS v2.0
    Programa casi listo solo falta poner el modulo de instrucciones.

---
+++ Dar de alta GPIO.py
@@ -0,0 +1,6 @@
+gpio_used = 1
+gpio_num = 4
+gpio_pin_1 = port:PG00<1><default><default><default>
+gpio_pin_2 = port:PB19<1><default><default><default>
+gpio_pin_3 = port:PG02<0><default><default><default>
+gpio_pin_4 = port:PG04<0><default><default><default>
