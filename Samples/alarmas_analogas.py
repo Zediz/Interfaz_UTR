3d62f6904f5d32a1c4c5a93d96d5af3cf90ef73b
Zediz <hiram.shock@gmail.com>
2014-04-27 22:12:36 -0500

    IDU - UM v1
    modificación del analogo

--- Samples/alarmas_analogas.py
+++ Samples/alarmas_analogas.py
@@ -1,3 +1,6 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*
+
 from Tkinter import *
 import time, os
 import thread
@@ -26,26 +29,43 @@ class alarmas_analogas(Frame):
 		self.valv = ""
 		self.valc = ""
 
+
 	def checando_analog(self): # Metodo que checa los archivos adc para ver los valores que estos tienen
 		global lock
+
+		ADC_PATH = os.path.normpath('/proc/')
+		ADC_FILENAME = "adc"
+
+		adcFiles=[]
+		conta = 0
+		x=0
+		punto = 0
+
+		for i in range(0,6): # Guardamos las direcciones de los archivos en un arreglo
+		    adcFiles.append(os.path.join(ADC_PATH, ADC_FILENAME+ str(i)))
 		
 		while True: # El bucle que se ejecuta para que continuamente se esten revisando el contenido de los archivos
 			lock.acquire()
 			#print("Se esta ejecutando el While Analogas")
 			time.sleep(.3)  # El tiempo entre revision y revision
-			file = open('/proc/adc3','r')
-			valor = file.read()
-			self.voltaje.config(text=valor)
-			self.valv=valor
-			file.close()
-			file2 = open ('/proc/adc4','r')
-			valor2= file2.read()
-			self.valc = valor2
-			self.corriente.config(text = valor2)
-			file2.close()
-			valor = ""
-			valor2 =""
-			 # de ahi en adelante lee el valor
+			for file in adcFiles: #Este abre los 6 archivos de los pines
+
+				fd = open(file,'r') # se abre el archivo
+				fd.seek(0) # Se situa en el character 5 del archivo
+				valor = fd.read() # de ahi en adelante lee el valor
+				
+
+				if conta == 3:
+					self.voltaje.config(text=valor+" V") # si esta en el pin 3 asigna el valor en la alarma analoga 1
+					self.valv = valor
+					#print self.valv
+				elif conta ==4: # si esta en el pin 4 asigna el valor a la label de la alarma analoga 2
+					self.corriente.config(text=valor+" mA")
+					self.valc = valor
+					#print self.valc
+				conta += 1 # aumenta conta para saber en que momento entra en el pin 3  y 4
+				fd.close() # Cierra el archivo cada vez que se termina de leer
+			conta = 0 # la variable conta la ponemos en 0 nuevamente apra que empiece a contar de nuevo
 			lock.release()
 
 	def dar_valv(self):
@@ -56,12 +76,10 @@ class alarmas_analogas(Frame):
 		numero = self.valc
 		return numero
 		
-v0 = Tk()
+"""v0 = Tk()
 v0.config(bg = "white")
 v0.title('Alarmas del Transformador')
 v0.geometry('700x500+290+150')
-
-
 
 ola = alarmas_analogas(v0)
 
